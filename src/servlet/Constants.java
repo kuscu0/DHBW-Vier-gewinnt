5806c4192572cd491a7d6fe4707fecedb2a5113b
kuscu0 <deniz@kuscu.de>
2020-06-19 12:09:52 +0200

    Sch*** encodings gefixt

--- src/servlet/Constants.java
+++ src/servlet/Constants.java
@@ -7,7 +7,7 @@ public class Constants
 	public static final String HTML_START = "<!DOCTYPE html>" +
             "<html>" +
         	"<head>" +
-        		"<meta charset=\"UTF-8\">" +
+        		"<meta charset=\"ISO-8859-1\">" +
         		"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>" +
         		"<link rel=\"stylesheet\" type=\"text/css\" href=\"css/style.css\">" +
         		"<link rel=\"shortcut icon\" href=\"favicon.ico\"/>" +
