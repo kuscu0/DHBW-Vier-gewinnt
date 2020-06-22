ca90675be16ca85ee570ca0e38e34fe4a7dca452
jakob.stegmueller <jakob.stegmueller@DESKTOP-2PDE99F.fritz.box>
2020-06-22 14:50:07 +0200

    animation

--- WebContent/js/display.js
+++ WebContent/js/display.js
@@ -1,5 +1,5 @@
 function removeAllChildNodes(parentNode) {
-    while (parentNode.firstchild) {
+    while (parentNode.firstChild) {
         parentNode.removeChild(parentNode.firstChild);
     }
 }
@@ -19,16 +19,20 @@ function createTable(tablaData, parentNode) {
         btn.name = ("insertBtn")
         btn.value = i
         form.appendChild(btn);
+        if(tablaData[0][i] != 0){
+        	disableButton(btn);
+        }
         tField.appendChild(form);
         tRow.appendChild(tField)
     }
     table.appendChild(tRow);
     var img;
-    for (i = 0; i < 6; i++) {
+    for (var i = 0; i < 6; i++) {
         tRow = document.createElement("tr");
 
-        for (j = 0; j < 7; j++) {
+        for (var j = 0; j < 7; j++) {
             tField = document.createElement("td")
+            tField.setAttribute("id" , String(i) + "," + String(j))
             img = document.createElement("img")
             img.setAttribute("src", getSource(tablaData[i][j]))
             img.setAttribute("style", "margin: 10px;");
@@ -49,6 +53,62 @@ function createTable(tablaData, parentNode) {
     table.setAttribute("style", "margin: 0 auto;" +
     							"background-color: #0000FF;" +
 								"border-radius: 20px;");
+    
+    var cord = getChanged(tablaData);
+    if (typeof cord !== 'undefined'){
+    	var cordStart = {
+    			xCord : cord.xCord,
+    			yCord : 0,
+    			value : cord.value,
+    	};
+    	var cordTmp = {
+    			xCord : cord.xCord,
+    			yCord : cord.yCord,
+    			value : 0,
+    	}
+    	setChip(cordTmp);
+    	animateFall(cord , cordStart)
+    }
+}
+
+function setChip(cord){
+	var element = document.getElementById(String(cord.yCord)+","+ String(cord.xCord));
+	if (element !== null){
+		removeAllChildNodes(element);
+	    var img = document.createElement("img");
+	    img.setAttribute("src", getSource(cord.value));
+	    img.setAttribute("style", "margin: 10px;");
+	    element.appendChild(img);
+	}
+}
+
+function animateFall(cordEnd , cordCurrent){
+	var cord = {
+			xCord : cordCurrent.xCord,
+			yCord : cordCurrent.yCord - 1,
+			value : 0
+	};
+	setChip(cord);
+	setChip(cordCurrent);
+	
+	if(cordEnd.yCord != cordCurrent.yCord){	
+		cordCurrent.yCord = (cordCurrent.yCord + 1);
+		setTimeout(function(){animateFall(cordEnd , cordCurrent); }, 300);
+	}
+}
+function getChanged(tableData){
+    for (var i = 0; i < 6; i++) {
+        for (var j = 0; j < 7; j++) {
+        	if(tableData[i][j] > 2){
+        		var cord = {
+        				xCord : j,
+        				yCord : i,
+        				value : (tableData[i][j] - 2)
+        		};
+        		return cord;
+        	}
+        }
+    }
 }
 
 function removeButton(id) {
@@ -56,11 +116,14 @@ function removeButton(id) {
     btn.remove();
 }
 
+function disableButton(element){
+	element.setAttribute("style", "border-top: 20px solid gray; cursor: not-allowed;");
+	element.disabled = true;
+}
+
 function removeButtons() {
     for (i = 0; i < 7; i++) {
-        var btn = document.getElementById("btn_spalte_" + i);
-        btn.setAttribute("style", "border-top: 20px solid gray; cursor: not-allowed;");
-        btn.disabled = true;
+    	disableButton(document.getElementById("btn_spalte_" + i));
     }
 }
 
